05d8cfd717a1c6bdf75d72538622a8c21995ba27
yang <ysotgood@gmail.com>
2023-05-31 15:35:18 +0900

    서비스, 컨트롤러 수정

--- JS/saleListController.js
+++ JS/saleListController.js
@@ -3,7 +3,50 @@ const saleListEventHandler = () => {
 
     saleList.addEventListener('click', () => {
         if (saleDB.get(1)) {
-            printSaleList();
+            const retArr = printSaleList();
+            retArr.forEach((sale, idx) => {
+                const tbody = document.querySelector('.tbody');
+                tbody.insertAdjacentHTML(
+                    'beforeend',
+                    `<tr>
+                    <td>${sale.customer[idx].c_id}</td>
+                    <td>${sale.customer[idx].c_name}</td>
+                    <td>${sale.customer[idx].c_mail}<td>
+                    </td>
+                    <td>
+                    ${sale.customer[idx].c_phone}
+                    </td>
+                    <td>
+                    ${sale.product[idx].p_no}
+                    </td>
+                    <td>
+                    ${sale.product[idx].p_name}
+                    </td>
+                    <td>${sale.product[idx].p_price}</td>
+                    <td>
+                    ${sale.product[idx].p_quantity}
+                    </td>
+                    <td>${sale.manufature[idx].m_no}</td>
+                    <td>
+                    ${sale.manufature[idx].m_name}
+                    </td>
+                    <td>
+                    ${sale.manufature[idx].m_address}
+                    </td>
+                    <td>
+                    ${sale.manufature[idx].m_phone}
+                    </td>
+                    <td>
+                    ${sale.manufature[idx].m_mananger}
+                    </td>
+                    <td>
+                    ${sale.value[idx].s_no}
+                    </td>
+                    <td> ${sale.value[idx].s_price}</td>
+                    <td> ${sale.value[idx].c_id}</td>
+                <tr>`
+                );
+            });
         }
     });
 };
--- JS/saleListServiec.js
+++ JS/saleListServiec.js
@@ -3,14 +3,12 @@ const printSaleList = () => {
     let customer;
     let product;
     let manufature;
-    let sale;
-    let i = 1;
+
     for (const value of saleDB.values()) {
         customer = customerDB.get(value.c_id);
         product = productDB.get(value.p_no);
         manufature = manufactureDB.get(product.m_no);
-        sale = saleDB.get(i++);
-        retArr.push({ customer, product, manufature, sale });
+        retArr.push({ customer, product, manufature, value });
     }
 
     return retArr;

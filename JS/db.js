c486c7a60b4891b25f09853cb97e199f0123c352
iolagvi28 <iolagvi28@gmail.com>
2023-05-31 12:58:57 +0900

    db 함수 생성!!

--- JS/db.js
+++ JS/db.js
@@ -3,4 +3,29 @@ const productDB = new Map();
 const manufactureDB = new Map();
 const saleDB = new Map();
 
+/* customerDB START */
+const searchCustomer = () => {
+  console.log("searchCustomer() CALLED!!");
 
+  //customerDumy를 customerDB에 저장
+  customerDB.set();
+};
+/* customerDB END */
+
+/* productDB START */
+const searchProduct = () => {
+  console.log("searchProduct() CALLED!!");
+
+  //productDumy를 productDB에 저장
+  productDB.set();
+};
+/* productDB END */
+
+/* manufactureDB START */
+const searchMmanufacturer = () => {
+  console.log("searchMmanufacturer() CALLED!!");
+
+  // manufactureDumy를 manufactureDB에 저장
+  manufactureDB.set();
+};
+/* manufactureDB END */

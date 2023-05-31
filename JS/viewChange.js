8be2541a226b84de9caa07c9eccd0b5f7dccbde5
siyeong1013 <tldud4346@gmail.com>
2023-05-31 12:54:41 +0900

    featuer 3 second update

--- JS/viewChange.js
+++ JS/viewChange.js
@@ -1,20 +1,13 @@
 const ADDSALE_VIEW = 1;
 const PRODUCTLIST_VIEW = 2;
-let signUpWrap = '';
-let signInWrap = '';
-let signModifyWrap=''
-let reservationWrap=''
-let reservationListWrap=''
+let addSaleWrap = '';
+let productListWrap = '';
 
-// sign_modify_wrap
 const initViews = () => {
     console.log('initViews() called!');
 
-    signUpWrap = document.querySelector('div.sign_up_wrap');
-    signInWrap = document.querySelector('div.sign_in_wrap');
-    signModifyWrap=document.querySelector('div.sign_modify_wrap');
-    reservationWrap=document.querySelector('div.reservation_wrap');
-    reservationListWrap=document.querySelector("div.reservationList_wrap");
+    addSaleWrap = document.querySelector('div.addSale_wrap');
+    productListWrap = document.querySelector('saleProductList_wrap');
 
 }
 
@@ -22,43 +15,14 @@ const showSelectedView = (viewNo) => {
     console.log('showSelectedView() called!');
 
     switch(viewNo) {
-        case SIGN_UP_VIEW:
-            signUpWrap.style.display = 'block';
-            signInWrap.style.display='none';
-            signModifyWrap.style.display="none";
-            reservationWrap.style.display='none'
-            reservationListWrap.style.display='none';
-         
+        case ADDSALE_VIEW:
+            addSaleWrap.style.display = 'block';
+            productListWrap.style.display='none';
             break;
-        case SIGN_IN_VIEW:
-        case SIGN_OUT_VIEW:
-            signUpWrap.style.display = 'none';
-            signInWrap.style.display='block';
-            signModifyWrap.style.display="none";
-            reservationWrap.style.display='none';
-            reservationListWrap.style.display='none';
-          
-            break;
-        case UPDATE_INFO:
-            signUpWrap.style.display = 'none';
-            signInWrap.style.display='none';
-            signModifyWrap.style.display="block";
-            reservationWrap.style.display='none';
-            reservationListWrap.style.display='none';
-            break;
-        case CREATE_RESERVATION:
-            signUpWrap.style.display = 'none';
-            signInWrap.style.display='none';
-            signModifyWrap.style.display="none";
-            reservationWrap.style.display='block';
-            reservationListWrap.style.display='none';
-            break;
-        case RESERVATION_LIST:
-            signUpWrap.style.display = 'none';
-            signInWrap.style.display='none';
-            signModifyWrap.style.display="none";
-            reservationWrap.style.display='none';
-            reservationListWrap.style.display='block';
+
+        case PRODUCTLIST_VIEW:
+            addSaleWrap.style.display = 'block';
+            productListWrap.style.display='none';
             break;
     }
 
